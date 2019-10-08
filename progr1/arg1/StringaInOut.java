ce071a6f61caf0f92733f53b48255b338a7e27e3
andreadlm <delmastro2000@gmail.com>
2019-10-08 15:00:35 +0200

    Esercizi arg1 del 08/10/2019

---
+++ progr1/arg1/StringaInOut.java
@@ -0,0 +1,7 @@
+class StringaInOut {
+    public static void main(String[] args) {
+        System.out.print("Inserire una stringa : ");
+        String str = SIn.readLine();
+        System.out.println("Hai inserito la stringa : " + str);
+    }
+}
---
+++ progr1/arg1/Supermarket.java
@@ -0,0 +1,38 @@
+/*Esercizio 1:
+
+calcolare il prezzo di quanto acquistiamo al supermercato:
+3 pacchi di orecchiette
+2 kg di pomodori
+
+prezzo di un pacco di orecchiette: euro 1.50
+prezzo al kg dei pomodori: euro 2.50
+
+Scrivere una classe SuperMarket il cui
+metodo main calcoli e visualizzi a video
+la spesa totale.
+
+Modellare il problema utilizzando delle variabili opportunamente dichiarate con tipi adeguati.
+Utilizzare una variabile anche per rappresentare il totale.
+Indentare e commentare opportunamente il programma.
+
+Infine, fare inserire le quantità degli articoli alimentari dall'utente da tastiera (anziche' fissare
+nel codice del programma i valori delle variabili).*/
+
+public class Supermarket {
+    public static void main(String[] args) {
+
+        // Dichiarazione costanti prezzi
+        final float P_ORECHHIETTE = 1.50f;
+        final float P_POMODORI = 2.50f;
+
+        // Input quantità acquistate
+        System.out.println("Inserire il numero di pacchi di orecchiette acqusitati : ");
+        int orecchiette = SIn.readInt();
+        System.out.println("Inserire il numero di chili di pomodori acqusitati : ");
+        int pomodori = SIn.readInt();
+
+        // Calcolo del totale e output
+        float tot = orecchiette * P_ORECHHIETTE + pomodori * P_POMODORI;
+        System.out.println("Il totale speso e' : " + tot);
+    }
+}
