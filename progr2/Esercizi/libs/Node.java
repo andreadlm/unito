f4d7b475057bea6fc4d035de5c7d3ef25939c1b8
andreadlm <delmastro2000@gmail.com>
2020-06-26 12:59:14 +0200

    Aggiunto metodo toString()

--- progr2/Esercizi/libs/Node.java
+++ progr2/Esercizi/libs/Node.java
@@ -35,4 +35,9 @@ public class Node<T> {
     public void setNext(Node<T> next) {
         this.next = next;
     }
+
+    @Override
+    public String toString() {
+        return "[" + elem + "]";
+    }
 }
